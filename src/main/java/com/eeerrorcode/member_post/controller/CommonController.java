acd901fa0b8d5f70903b8fadc3c21d72c899a492
jungyoonn <sophia7625@naver.com>
2024-12-17 18:12:10 +0900

    공통 alert

--- src/main/java/com/eeerrorcode/member_post/controller/CommonController.java
+++ src/main/java/com/eeerrorcode/member_post/controller/CommonController.java
@@ -1,7 +1,10 @@
 package com.eeerrorcode.member_post.controller;
 
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CommonController {
@@ -9,5 +12,10 @@ public class CommonController {
   @GetMapping({"/", "/index"})
   public String index() {
     return "common/index";
+  }
+
+  @RequestMapping(value = "msg")
+  public String msg(@ModelAttribute("msg") String msg, @ModelAttribute("url") @Nullable String url) {
+      return "common/msg";
   }
 }
