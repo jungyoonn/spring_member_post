7dea2144f8686fb9ad4db5aef9b706f91164c597
jungyoonn <sophia7625@naver.com>
2024-12-17 11:58:04 +0900

    게시글 수정, 삭제

--- src/main/java/com/eeerrorcode/member_post/controller/PostController.java
+++ src/main/java/com/eeerrorcode/member_post/controller/PostController.java
@@ -12,11 +12,14 @@ import com.eeerrorcode.member_post.service.PostService;
 import com.eeerrorcode.member_post.vo.Post;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequestMapping("post")
 @AllArgsConstructor
+@Log4j2
 public class PostController {
   private PostService service;
 
@@ -39,6 +42,25 @@ public class PostController {
     post.setCno(cri.getCategory());
     service.write(post);
     return "redirect:list?" + cri.getQs2();
+  }
+  
+  @GetMapping("modify")
+  public void modify(Long pno, Model model, Criteria cri) {
+    log.info(pno);
+    model.addAttribute("post", service.findBy(pno));
+  }
+  
+   @PostMapping("modify")
+   public String postMethodName(Post post, Criteria cri) {
+    log.info(post);
+    service.modify(post);
+    return "redirect:list?" + cri.getQs();
+  }
+   
+  @RequestMapping("remove")
+  public String requestMethodName(Long pno, Criteria cri) {
+    service.remove(pno);
+    return "redirect:list?" + cri.getQs();
   }
   
 }
