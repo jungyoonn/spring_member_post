cb64d277afc86a26f237b7f43b54b51f6cc61b65
jungyoonn <sophia7625@naver.com>
2024-12-17 18:25:47 +0900

    로그인 페이지로 보내기

--- src/main/java/com/eeerrorcode/member_post/controller/PostController.java
+++ src/main/java/com/eeerrorcode/member_post/controller/PostController.java
@@ -3,14 +3,19 @@ package com.eeerrorcode.member_post.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import com.eeerrorcode.member_post.aop.MyPost;
+import com.eeerrorcode.member_post.aop.SigninCheck;
 import com.eeerrorcode.member_post.dto.Criteria;
 import com.eeerrorcode.member_post.dto.PageDto;
 import com.eeerrorcode.member_post.service.PostService;
+import com.eeerrorcode.member_post.vo.Member;
 import com.eeerrorcode.member_post.vo.Post;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
@@ -35,6 +40,7 @@ public class PostController {
   }
 
   @GetMapping("write")
+  @SigninCheck
   public void write(@ModelAttribute("cri") Criteria cri) {}
   
   @PostMapping("write")
@@ -45,22 +51,30 @@ public class PostController {
   }
   
   @GetMapping("modify")
-  public void modify(Long pno, Model model, Criteria cri) {
+  @SigninCheck
+  public void modify(Long pno, Model model, Criteria cri, HttpSession session,
+   @SessionAttribute(required = false) Member member, String writer) {
     log.info(pno);
+    Post post = service.findBy(pno);
+    if(member == null || !post.getWriter().equals(member.getId())) {
+      throw new RuntimeException("아이디가 일치하지 않거나 로그인하지 않은 상태입니다.");
+    }
     model.addAttribute("post", service.findBy(pno));
   }
   
    @PostMapping("modify")
-   public String postMethodName(Post post, Criteria cri) {
+   @SigninCheck @MyPost
+   public String postModify(Post post, Criteria cri) {
+
     log.info(post);
     service.modify(post);
     return "redirect:list?" + cri.getQs();
   }
+
    
   @RequestMapping("remove")
-  public String requestMethodName(Long pno, Criteria cri) {
+  public String remove(Long pno, Criteria cri) {
     service.remove(pno);
     return "redirect:list?" + cri.getQs();
   }
-  
 }
