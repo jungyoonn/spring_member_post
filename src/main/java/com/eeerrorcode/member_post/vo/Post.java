b98c16397221f8ab202ddbbc91c555231582e933
jungyoonn <sophia7625@naver.com>
2024-12-18 16:12:28 +0900

    생성자 하나 삭제, 날짜 포맷터 추가

--- src/main/java/com/eeerrorcode/member_post/vo/Post.java
+++ src/main/java/com/eeerrorcode/member_post/vo/Post.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import java.util.List;
 
 import org.apache.ibatis.type.Alias;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.*;
 
@@ -16,6 +17,7 @@ public class Post {
 	private String writer;
 	private String content;
 	private Long viewCount;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date regdate;
 	private Date updatedate;
 	private Integer cno;
@@ -23,18 +25,18 @@ public class Post {
 
 	private List<Attach> attachs = new ArrayList<Attach>();
 	
-	public Post(Long pno, String title, String writer, String content, Long viewCount, Date regdate, Date updatedate,
-			Integer cno, Boolean attachFlag) {
-		super();
-		this.pno = pno;
-		this.title = title;
-		this.writer = writer;
-		this.content = content;
-		this.viewCount = viewCount;
-		this.regdate = regdate;
-		this.updatedate = updatedate;
-		this.cno = cno;
-		this.attachFlag = attachFlag;
-	}
+	// public Post(Long pno, String title, String writer, String content, Long viewCount, Date regdate, Date updatedate,
+	// 		Integer cno, Boolean attachFlag) {
+	// 	super();
+	// 	this.pno = pno;
+	// 	this.title = title;
+	// 	this.writer = writer;
+	// 	this.content = content;
+	// 	this.viewCount = viewCount;
+	// 	this.regdate = regdate;
+	// 	this.updatedate = updatedate;
+	// 	this.cno = cno;
+	// 	this.attachFlag = attachFlag;
+	// }
 	
 }
