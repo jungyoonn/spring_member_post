4b07802b654e5eece0c01519610f6eff27a9e8ef
jungyoonn <sophia7625@naver.com>
2024-12-19 14:53:13 +0900

    Mypost 내 게시글 확인 aop 구현

--- src/main/java/com/eeerrorcode/member_post/aop/AuthAspect.java
+++ src/main/java/com/eeerrorcode/member_post/aop/AuthAspect.java
@@ -9,7 +9,10 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
+import com.eeerrorcode.member_post.exception.NotMyPostException;
+import com.eeerrorcode.member_post.exception.UnsignedAuthException;
 import com.eeerrorcode.member_post.vo.Member;
+import com.eeerrorcode.member_post.vo.Post;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -26,23 +29,7 @@ public class AuthAspect {
   private HttpServletResponse resp;
   private HttpServletRequest req;
 
-  @Before("@annotation(com.eeerrorcode.member_post.aop.MyPost)")
-  public void myPost(JoinPoint joinPoint, MyPost myPost){
-    Object o = session.getAttribute("member");
-    String id = ((Member) o).getId();
-
-    Object[] args = joinPoint.getArgs();
-    String writeParam = myPost.value();
-    log.info(Arrays.toString(args));
-    log.info(id);
-    log.info(writeParam);
-
-    // if(o == null || ((Member)o).getId().equals(post.getWriter())) {
-    //   throw new RuntimeException("본인 게시글 아님");
-    // }
-  }
-
-  @Before("@annotation(com.eeerrorcode.member_post.aop.SigninCheck)")
+  @Before("@annotation(com.eeerrorcode.member_post.aop.annotation.SigninCheck)")
   public void signinCheck(JoinPoint jp) throws IOException {
     log.info(req.getRequestURL());
     log.info(req.getRequestURI());
@@ -51,5 +38,28 @@ public class AuthAspect {
       String url = "/member/signin?url=" + URLEncoder.encode(req.getRequestURI() + "?" + req.getQueryString(), "utf-8");
       resp.sendRedirect("/msg?msg=" + URLEncoder.encode("로그인이 필요한 서비스입니다.", "utf-8") + "&url=" + url);
     }
+  }
+
+  @Before("@annotation(com.eeerrorcode.member_post.aop.annotation.MyPost)")
+  public void myPost(JoinPoint joinPoint){
+    log.info("진입했음");
+    Object o = session.getAttribute("member");
+
+    if(o == null) {
+      throw new UnsignedAuthException("로그인이 필요한 서비스입니다");
+    }
+    
+    String id = ((Member) o).getId();
+
+    Object[] args = joinPoint.getArgs();
+    for(Object obj : args) {
+      if(obj instanceof Post && !((Post)obj).getWriter().equals(id)) {
+        throw new NotMyPostException("본인이 작성한 게시글이 아닙니다");
+      }
+    }
+    log.info(Arrays.toString(args));
+    log.info(id);
+
+    log.info("메서드 끝");
   }
 }
