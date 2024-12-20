0339db73356afd62d2b3dd18ca8b9775ca09602b
jungyoonn <sophia7625@naver.com>
2024-12-20 15:18:25 +0900

    태스크 비활성화

--- src/main/java/com/eeerrorcode/member_post/task/MyTask.java
+++ src/main/java/com/eeerrorcode/member_post/task/MyTask.java
@@ -5,7 +5,6 @@ import java.util.Date;
 
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.log4j.Log4j2;
@@ -17,7 +16,7 @@ public class MyTask {
 
   // @Scheduled가 지정된 메서드는 파라미터 x, 반환 값 x
   @Async
-  @Scheduled(cron = "0/5 * * * * *")
+  // @Scheduled(cron = "0/5 * * * * *")
   public void printTime() {
     SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd - hh:mm:ss:SSS");
     Date date = new Date();
