97e2768bd3bf8e84448cb1742429d2833b21138a
jungyoonn <sophia7625@naver.com>
2024-12-20 13:24:09 +0900

    파일 지우기, 썸네일 지우기 테스트

--- src/test/java/com/eeerrorcode/member_post/mapper/AttachMapperTests.java
+++ src/test/java/com/eeerrorcode/member_post/mapper/AttachMapperTests.java
@@ -29,9 +29,44 @@ public class AttachMapperTests {
     log.info("=============================================");
     List<File> files = new ArrayList<>(Arrays.asList(new File("c:/upload", date).listFiles()));
     files.forEach(log::info);
+    log.info(files.size());
     
     log.info("=============================================");
     List<File> list2 = list.stream().map(Attach::toFile).toList();
-    log.info(list2);
+    log.info(list2.size());
+
+    files.removeAll(list2);
+    files.forEach(log::info);
+    log.info(files.size());
+  }
+
+  @Test
+  public void fromFileTest() {
+    String date = "2024/12/19";
+    List<Attach> files = new ArrayList<>(Arrays.asList(new File("c:/upload", date).listFiles())
+    .stream()
+    .map(Attach::fromFile)
+    .toList());
+
+    files.forEach(log::info);
+
+    List<Attach> dbs = new ArrayList<>(mapper.selectListByPath(date));
+    List<Attach> thumbs = new ArrayList<>(dbs);
+    thumbs = dbs.stream().filter(Attach::isImage)
+    .map(a -> Attach.builder().uuid("t_" + a.getUuid()).build())
+    .toList();
+    
+    log.info(thumbs);
+    log.info(thumbs.size());
+
+    dbs.addAll(thumbs);
+    log.info("dbs는 ::: " + dbs);
+    log.info(dbs.size());
+
+    log.info("=============================================");
+    log.info(files.size());
+    files.removeAll(dbs);
+    files.forEach(log::info);
+    log.info(files.size());
   }
 }
