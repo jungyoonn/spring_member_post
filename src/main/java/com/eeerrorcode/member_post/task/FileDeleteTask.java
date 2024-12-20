1109c057517537607c5551e44a6de19597311a30
jungyoonn <sophia7625@naver.com>
2024-12-20 15:18:52 +0900

    파일 비교 후 삭제 태스크

--- src/main/java/com/eeerrorcode/member_post/task/FileDeleteTask.java
+++ src/main/java/com/eeerrorcode/member_post/task/FileDeleteTask.java
@@ -22,7 +22,7 @@ public class FileDeleteTask {
   @Autowired
   private AttachMapper mapper;
 
-  @Scheduled(cron = "0 0 14 * * *")
+  @Scheduled(cron = "0 35 14 * * *")
   public void deleteFiles() {
     String date = "2024/12/19";
 
@@ -34,14 +34,19 @@ public class FileDeleteTask {
     List<Attach> dbs = new ArrayList<>(mapper.selectListByPath(date));
     List<Attach> thumbs = new ArrayList<>(dbs);
     thumbs = dbs.stream().filter(Attach::isImage)
+    
     .map(a -> Attach.builder().uuid("t_" + a.getUuid()).build())
     .toList();
-    
     dbs.addAll(thumbs);
     files.removeAll(dbs);
 
-    files.stream().map(Attach::toFile).forEach(File::delete);
+    files.stream().peek(a -> a.setPath(date)).map(Attach::toFile).forEach(File::delete);
 
     log.info(files.size() + "개의 파일 삭제");
+
+    // stream의 최종 연산 : forEach(파라미터 consumer), count(스트림의 개수 리턴, 파라미터 없음), Collect(Collectors.toList()) <- 얘는 그냥 외우기
+
+    // stream의 중간 연산 : 최종 연산이 뒤에 안 붙으면 중간 연산이 먹지 않음, 반환 타입이 모두 스트림
+    // filter(파라미터 predicaate), map(파라미터 function), peek(파라미터 consumer)
   }
 }
